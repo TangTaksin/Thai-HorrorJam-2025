d0522aa53e52a75be4dec32a95e05eac5a1499a1
Tang <taksin_tang51@hotmail.com>
2025-10-30 00:30:47 +0700

    [Fix] mouse cursor mainmenu

--- Assets/Scripts/GameStates/GameManager.cs
+++ Assets/Scripts/GameStates/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        ChangeState(GameState.Playing);
+        ChangeState(GameState.MainMenu);
     }
 
     private void Update()
